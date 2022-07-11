ad73dddd74149bc891d33c07f9d59e3b27b043d9
carella11 <christianar@getacademy.no>
2022-07-11 15:00:44 +0200

    Add project files.

---
+++ TerjeOppgave1/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TerjeOppgave1
+{
+    public class Data
+    {
+        public string Date { get; private set; }
+        public double Distance { get; private set; }
+        
+
+        public Data (string date, double distance)
+        {
+            Date = date;
+            Distance = distance;
+        }
+
+        public void printInfo(int id)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine($"Walk {id}");
+            Console.WriteLine($"Date: {Date}");
+            Console.WriteLine($"Distance {Distance} km");
+        }
+    }
+}
---
+++ TerjeOppgave1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TerjeOppgave1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Day1 = new Data("2022-07-01", 5.3);
+            var Day2 = new Data("2022-07-02", 1.2);
+            var Day3 = new Data("2022-07-05", 7.5);
+
+            var totalDistance = Day1.Distance + Day2.Distance + Day3.Distance;
+
+            var ListWalkData = new List<Data>();
+            ListWalkData.Add(Day1);
+            ListWalkData.Add(Day2);
+            ListWalkData.Add(Day3);
+
+            var id = 1;
+            foreach (var Walk in ListWalkData)
+            {
+                Walk.printInfo(id);
+                id++;
+            }
+
+            Console.WriteLine("*********************");
+            Console.WriteLine($"Nice Work! You've Walked: {totalDistance} km");
+        }
+    }
+}
