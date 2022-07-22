4d9ac24fa5ac8922db3e3d31fd2d15c8fce1f996
carella11 <97164959+carella11@users.noreply.github.com>
2022-07-22 12:53:25 +0200

    Add files via upload
    New Version with .txt file that updates and saves.

---
+++ Data.cs
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+﻿using System;
+
+namespace TerjeOppgave1
+{
+    public class Data
+    {
+        public string Date { get; private set; }
+        public double Distance { get; private set; }
+
+        public Data (string date, double distance)
+        {
+            Date = date;
+            Distance = distance;
+        }
+        public void printInfo(int id)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine($"Walk {id}");
+            Console.WriteLine($"Date: {Date}");
+            Console.WriteLine($"Distance: {Distance} km");
+        }
+
+        public double CalculateDistance()
+        {
+            return Distance;
+        }
+    }
+=======
+﻿using System;
+
+namespace TerjeOppgave1
+{
+    public class Data
+    {
+        public string Date { get; private set; }
+        public double Distance { get; private set; }
+
+        public Data (string date, double distance)
+        {
+            Date = date;
+            Distance = distance;
+        }
+        public void printInfo(int id)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine($"Walk {id}");
+            Console.WriteLine($"Date: {Date}");
+            Console.WriteLine($"Distance: {Distance} km");
+        }
+
+        public double CalculateDistance()
+        {
+            return Distance;
+        }
+    }
+>>>>>>> 150dfc1e6ca3ea5bdd1d440cd0e80632ac887a92
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TerjeOppgave1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var WalkHistoryPage = CreateWalkHistoryPage();
+
+            while (true)
+            {
+                MenuText.ShowMenuText(WalkHistoryPage);
+            }
+        }
+        public static WalkHistoryPage CreateWalkHistoryPage()
+        {
+            string filePath = "C:\\Users\\GET Academy\\Desktop\\Modul 3 Prosjekter\\txtFilTerjeOppg\\txtFilTerjeOppg\\bin\\Debug\\net5.0\\myWalks.txt";
+            var WalkHistoryPage = new WalkHistoryPage();
+             
+            foreach (string s in File.ReadLines(filePath))
+            {
+                string[] userInput = s.Split(',');
+                var stringDouble = userInput[1];
+                var kmWalked = Double.Parse(stringDouble, System.Globalization.CultureInfo.InvariantCulture);
+                WalkHistoryPage.AddWalk(userInput[0], kmWalked);
+            }
+            return WalkHistoryPage;
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+
+namespace TerjeOppgave1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var WalkHistoryPage = new WalkHistoryPage();
+            WalkHistoryPage.AddWalk("2022-07-01", 5.3);
+            WalkHistoryPage.AddWalk("2022-07-02", 1.2);
+            WalkHistoryPage.AddWalk("2022-07-05", 7.5);
+            
+            while (true)
+            {
+                MenuText.ShowMenuText(WalkHistoryPage);
+            }
+        }
+    }
+}
+>>>>>>> 150dfc1e6ca3ea5bdd1d440cd0e80632ac887a92
